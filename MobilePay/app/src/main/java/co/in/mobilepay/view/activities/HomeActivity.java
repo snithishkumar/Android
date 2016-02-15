a392e6fc9d158ae61e6b826c844f577206725093
ramachandran <ramachandran@knetworks.co.in>
2016-02-16 01:14:45 +0530

    #Home screen added

---
+++ MobilePay/app/src/main/java/co/in/mobilepay/view/activities/HomeActivity.java
@@ -0,0 +1,75 @@
+package co.in.mobilepay.view.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.in.mobilepay.R;
+import co.in.mobilepay.view.PurchaseModel;
+import co.in.mobilepay.view.fragments.PurchaseItemsFragment;
+
+public class HomeActivity extends AppCompatActivity implements PurchaseItemsFragment.OnListFragmentInteractionListener{
+    private TabLayout tabLayout = null;
+    private ViewPager viewPager = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new PurchaseItemsFragment(), "Home");
+        adapter.addFragment(new PurchaseItemsFragment(), "History");
+        adapter.addFragment(new PurchaseItemsFragment(), "Payment");
+        viewPager.setAdapter(adapter);
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    @Override
+    public void onListFragmentInteraction(PurchaseModel item) {
+
+    }
+}
