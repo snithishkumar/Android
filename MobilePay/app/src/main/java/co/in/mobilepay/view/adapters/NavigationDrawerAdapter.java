8a34435b0d6c2e8f918ce35155fa7f992ff57352
nithish-knetworks <nithish@knetworks.co.in>
2016-07-07 18:50:28 +0530

    Icon changes

--- MobilePay/app/src/main/java/co/in/mobilepay/view/adapters/NavigationDrawerAdapter.java
+++ MobilePay/app/src/main/java/co/in/mobilepay/view/adapters/NavigationDrawerAdapter.java
@@ -1,10 +1,12 @@
 package co.in.mobilepay.view.adapters;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
  
 import java.util.Collections;
@@ -42,6 +44,27 @@ public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDraw
     public void onBindViewHolder(MyViewHolder holder, int position) {
         String current = data.get(position);
         holder.title.setText(current);
+        switch (position){
+            case 0:
+                holder.imageView.setBackground(ContextCompat.getDrawable(context,R.drawable.ic_supervisor_account_black_24dp));
+                break;
+            case 1:
+                holder.imageView.setBackground(ContextCompat.getDrawable(context,R.drawable.ic_help_outline_black_24dp));
+                break;
+            case 2:
+                holder.imageView.setBackground(ContextCompat.getDrawable(context,R.drawable.ic_toc_black_24dp));
+                break;
+            case 3:
+                holder.imageView.setBackground(ContextCompat.getDrawable(context,R.drawable.ic_info_black_24dp));
+                break;
+            case 4:
+                holder.imageView.setBackground(ContextCompat.getDrawable(context,R.drawable.ic_stars_black_24dp));
+                break;
+            default:
+                break;
+
+        }
+
     }
  
     @Override
@@ -51,10 +74,12 @@ public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDraw
  
     class MyViewHolder extends RecyclerView.ViewHolder {
         TextView title;
+        ImageView imageView;
  
         public MyViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.title);
+            imageView = (ImageView)itemView.findViewById(R.id.navi_drawer_img);
         }
     }
 }
